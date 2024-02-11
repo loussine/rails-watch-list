4b0cd6fa2f325f852f0c9ea09af7e0e208134a4d
Yolande Touchet <yolandetouchet@hotmail.com>
2024-02-11 23:21:45 +0100

    Rails 4 - Watch List - Front en cours

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   root "lists#index"
 
   resources :lists, only: [:index, :show, :new, :create] do
-    resources :bookmarks, only: [:new, :create]
+    resources :bookmarks, only: [:new, :create, :show]
   end
   resources :bookmarks, only: :destroy
 end
