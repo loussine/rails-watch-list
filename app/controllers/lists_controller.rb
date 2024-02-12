2b422a708c69fbae3d52c2c167a53f0ad66af84f
Yolande Touchet <yolandetouchet@hotmail.com>
2024-02-12 04:19:59 +0100

    Rails4 - Watch List - Front Ok - Tentative de Seed et drop db

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -21,9 +21,15 @@ class ListsController < ApplicationController
     end
   end
 
+  def destroy
+    @list = List.find(params[:id])
+    @list.destroy
+    redirect_to lists_path, notice: 'List successfully destroyed'
+  end
+
   private
 
   def list_params
-    params.require(:list).permit(:name)
+    params.require(:list).permit(:name, :image_link)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   # root "posts#index"
   root "lists#index"
 
-  resources :lists, only: [:index, :show, :new, :create] do
+  resources :lists, only: [:index, :show, :new, :create, :destroy] do
     resources :bookmarks, only: [:new, :create, :show]
   end
   resources :bookmarks, only: :destroy
